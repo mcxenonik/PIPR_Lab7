9a27508ecca74897bbf8796c87f06f3cf9df684e
mcxenonik <01112955@pw.edu.pl>
2021-03-23 17:40:48 +0100

    Implemented unittests to Person() class

--- definitions.py
+++ definitions.py
@@ -1,7 +1,8 @@
 class Person:
-    def __init__(self, fname, lname):
+    def __init__(self, fname, lname, age=0):
         self.first_name = fname
         self.last_name = lname
+        self.age = age
 
     def introduce(self):
         return f'My name is {self.first_name} My last name is {self.last_name}'
---
+++ test_person.py
@@ -0,0 +1,15 @@
+from definitions import Person
+
+
+def test_init():
+    person = Person('First', 'Last')
+    assert person.first_name == 'First'
+    assert person.last_name == 'Last'
+    assert person.age == 0
+
+
+def test_init_with_age():
+    person = Person('First', 'Last', 18)
+    assert person.first_name == 'First'
+    assert person.last_name == 'Last'
+    assert person.age == 18
